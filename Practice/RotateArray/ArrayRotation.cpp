2ad454e2cdfaee2bd494daa7a09ce9851000d58a
Digvijay Srivastava <55308273+dig9074vijay@users.noreply.github.com>
2021-02-23 16:20:20 +0530

    Create ArrayRotation.cpp

---
+++ Practice/RotateArray/ArrayRotation.cpp
@@ -0,0 +1,31 @@
+
+// Code to rotate an array in the right direction
+
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false); 
+	cin.tie(NULL); 
+	long int num[100000],i, t, n, k,j;
+	cin >> t;
+	while(t--){
+		cin >> n >> k;
+		 k %= n;
+		for(i=0;i<n;i++)
+			cin >> num[i];
+		for(i=0;i<k;i++){
+			int l = num[n-1];
+			for(j=n-2;j>=0;j--)
+				num[j+1]=num[j];
+			num[0] = l;	
+		}
+		for(i=0;i<n;i++)
+			cout << num[i] << " ";
+		cout<<"\n";	
+	}	
+	return 0;
+}
+
+
