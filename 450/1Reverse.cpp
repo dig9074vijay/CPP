4f0d70e6dcacbad99b0f9c076ced20ce78f28bb9
Digvijay Srivastava <55308273+dig9074vijay@users.noreply.github.com>
2021-05-27 23:24:51 +0530

    Create 1Reverse.cpp

---
+++ 450/1Reverse.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a[20],n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int high , low;
+    high=n-1;low=0;
+    while(low<high){
+        a[low] ^= a[high];
+        a[high] ^= a[low];
+        a[low] ^= a[high];
+        low++;
+        high--;
+
+    }
+    for(int i=0;i<n;i++){
+        cout<<a[i]<<"\t";
+    }
+    return 0;
+}
