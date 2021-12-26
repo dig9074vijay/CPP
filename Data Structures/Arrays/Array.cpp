21f27d58b7b9f8e48f488312669b39ff55bb8ca7
Digvijay Srivastava <dig9074vijay@gmail.com>
2021-12-27 00:53:36 +0530

    Array basics

---
+++ Data Structures/Arrays/Array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int a[100];
+    // int a[100] = {0};
+    // int a[100] = {1,2,3};
+    // int a[] = {1,2,3};
+    string a[4] = {"mango","apple","guava"};
+    //int a[10];
+    for (int i = 0 ; i< 4;i++)
+    cout<<a[i];
+    return 0;
+}
