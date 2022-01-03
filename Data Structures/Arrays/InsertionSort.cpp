16b145823641660732cb23023a27d86090aa7f47
Digvijay Srivastava <dig9074vijay@gmail.com>
2022-01-03 18:31:39 +0530

    Added insertionsort

---
+++ Data Structures/Arrays/InsertionSort.cpp
@@ -0,0 +1,28 @@
+//bruteforce approach
+//O(N^3)
+#include<iostream>
+using namespace std;
+
+void InsertionSort(int a[],int n){
+
+    for(int times = 1; times<=n-1;times++){
+        int current = a[times];
+        int prev = times-1;
+        while(prev>=0 && a[prev]>current ){
+            a[prev+1] = a[prev];
+            prev = prev-1;
+        }
+        a[prev+1] = current;
+    }
+
+}
+int main(){
+    int arr[] = {-2,3,4,-1,5,-12,6,1,3};
+    int n  = sizeof(arr)/sizeof(int);
+    //cout<<largestSubarraySum(arr,n);
+    InsertionSort(arr,n);
+    for(int x : arr){
+        cout<<x<<",";
+    }
+    return 0;
+} 
