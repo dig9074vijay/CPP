226a79ae39d8dece658c0cf0968da98e879a690f
Digvijay Srivastava <dig9074vijay@gmail.com>
2022-01-03 16:35:58 +0530

    Added Kadanes

---
+++ Data Structures/Arrays/Kadanes.cpp
@@ -0,0 +1,25 @@
+//bruteforce approach
+//O(N^3)
+#include<iostream>
+using namespace std;
+
+int largestSubarraySum(int arr[],int n){
+    int largestSum = 0;
+    int cs =0;
+    for(int i =0;i<n;i++){
+        cs = cs + arr[i];
+        if(cs<0)
+            cs =0;
+        largestSum = max(largestSum,cs);    
+    }
+    return largestSum;
+}
+
+int main(){
+    int arr[] = {-2,3,4,-1,5,-12,6,1,3};
+    int n  = sizeof(arr)/sizeof(int);
+    //cout<<largestSubarraySum(arr,n);
+    cout<<largestSubarraySum(arr,n);
+
+    return 0;
+} 
--- Data Structures/Arrays/SubarraySum.cpp
+++ Data Structures/Arrays/SubarraySum.cpp
@@ -17,10 +17,29 @@ int largestSubarraySum(int arr[],int n){
     return largestSum;
 }
 
+//Prefix Sum O(n^2)
+int largestSubarraySum2(int arr[],int n){
+    int largestSum = 0;
+    int prefix[100] = {0};
+    prefix[0] = arr[0];
+    for(int i = 1;i < n;i++){
+        prefix[i] = prefix[i-1] + arr[i];
+    }
+    for (int i = 0;i<n;i++){
+        for(int j=i;j<n;j++){
+            int currentSum = i>0? prefix[j]-prefix[i-1]:prefix[j];
+            
+            largestSum = max(largestSum,currentSum);
+        }
+    }
+    return largestSum;
+}
 
 int main(){
     int arr[] = {-2,3,4,-1,5,-12,6,1,3};
     int n  = sizeof(arr)/sizeof(int);
-    cout<<largestSubarraySum(arr,n);
+    //cout<<largestSubarraySum(arr,n);
+    cout<<largestSubarraySum2(arr,n);
+
     return 0;
 } 
