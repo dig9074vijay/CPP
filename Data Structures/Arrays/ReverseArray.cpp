1a293d13bdec55c1e9f7ceae2a52148ec51ebdd9
Digvijay Srivastava <dig9074vijay@gmail.com>
2022-01-03 15:45:20 +0530

    Added Reverse Array

---
+++ Data Structures/Arrays/ReverseArray.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+void ReverseArray(int arr[],int n){
+    int s = 0;
+    int e = n-1;
+    while(s<e){
+        swap(arr[s],arr[e]);
+        s++;
+        e--;
+    }
+}
+
+int main(){
+    int arr[] = {432,5,32,7,8,2,5,2};
+    int n = sizeof(arr)/sizeof(int);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+
+    }
+    cout<<endl;
+    ReverseArray(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+
+    }
+    return 0;
+}
